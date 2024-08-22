772ab864be8672458c0f4fca667b9b7f078a3533
Zachary M Bailey <zbailey2@my.westga.edu>
2024-08-22 15:50:23 -0400

    Setup Project Repos
    Added starter project and setup basic project repos (code for files and README).

---
+++ code/Lab 1/src/edu/westga/cs1302/lab1/model/Bill.java
@@ -0,0 +1,63 @@
+package edu.westga.cs1302.lab1.model;
+
+import java.util.ArrayList;
+
+/** Stores information for a bill.
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class Bill {
+	private ArrayList<BillItem> items;
+	
+	/** Create a new empty Bill
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 */
+	public Bill() {
+		this.items = new ArrayList<BillItem>();
+	}
+	
+	/** Adds the item to the bill
+	 * 
+	 * @precondition item != null
+	 * @postcondition item is added to the list of items in the bill
+	 * 
+	 * @param item the item to be added to the bill
+	 */
+	public void addItem(BillItem item) {
+		if (item == null) {
+			throw new IllegalArgumentException("item must not be null.");
+		}
+		this.items.add(item);
+	}
+	
+	/** Return a String containing the list of bill items and total for the bill.
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @return a String containing the list of bill items and total for the bill
+	 */
+	public String getText() {
+		String text = "ITEMS" + System.lineSeparator();
+		double subTotal = 0.0;
+		for (BillItem item : this.items) {
+			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+			subTotal += item.getAmount();
+		}
+		
+		text += System.lineSeparator();
+		text += "SUBTOTAL - $" + subTotal + System.lineSeparator();
+		double tax = subTotal * 0.1;
+		double tip = subTotal * 0.2;
+		text += "TAX - $" + tax + System.lineSeparator();
+		text += "TIP - $" + tip + System.lineSeparator();
+		text += "TOTAL - $" + (subTotal + tip + tax);
+		
+		return text;
+	}
+
+}
